f555729a03c905c874e30f75d78f884e29d5b1e2
Rachel <rachel.sun.rondon@gmail.com>
2026-02-23 17:46:30 -0500

    feat: update dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -3,8 +3,8 @@ import './Dropdown.css';
 
 const Dropdown = () => {
 
-  const listOfColors = ["blue", "green"];
-  const [selectedColor, setSelectedColor] = useState("blue");
+  const listOfColors = ["light", "dark", "auto"];
+  const [selectedColor, setSelectedColor] = useState("light");
   const className = `layout ${selectedColor}`;
 
   const updateColor = (color) => {
@@ -14,7 +14,7 @@ const Dropdown = () => {
   return (
     <div className={className}>
       <label>
-        COLOR:
+        Appearance: 
         <select className="dropdown" name="selectColor" onChange={((e) => updateColor(e.target.value))}>
           {listOfColors.map((color) => {
             return (
